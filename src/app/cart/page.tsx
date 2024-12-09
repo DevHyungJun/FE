bb10902943f748ff0303b08eacd0a9dcc99843b6
jun <znznun@gmail.com>
2024-12-09 22:47:39 +0900

    상품평 정렬 구현 완료(구현 테스트 필요함)

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import useGetCart from "@/hooks/useGetCart";
 import CartProductDetail from "../components/cartProductDetail";
@@ -10,7 +10,7 @@ import useRemoveCart from "@/hooks/useRomoveCart";
 import { useQueryClient } from "@tanstack/react-query";
 import Link from "next/link";
 import { CiShoppingCart } from "react-icons/ci";
-import { IoHeartSharp } from 'react-icons/io5';
+import { IoHeartSharp } from "react-icons/io5";
 import useOrder from "@/hooks/useOrder";
 import { useRouter } from "next/navigation";
 import Favorite from "../components/Favorite";
@@ -21,20 +21,22 @@ export default function Cart() {
   const router = useRouter();
   const { data: cartData, isLoading } = useGetCart();
   const [selectedItems, setSelectedItems] = useState<string[]>([]);
-  const [prices, setPrices] = useState<{ id: string, price: number }[]>([]);
+  const [prices, setPrices] = useState<{ id: string; price: number }[]>([]);
   const totalPrice = prices
-    .filter(priceObj => selectedItems.includes(priceObj.id))
+    .filter((priceObj) => selectedItems.includes(priceObj.id))
     .reduce((acc, curr) => acc + curr.price, 0);
   const { mutate: removeMutate } = useRemoveCart();
   const { mutate: orderMutate } = useOrder();
-  const [cartOrFavorite, setCartOrFavorite] = useState('cart');
+  const [cartOrFavorite, setCartOrFavorite] = useState("cart");
   const { data: likeList, isLoading: likeLoading } = useLikeList();
 
   const handleAllCheckClick = () => {
     if (selectedItems.length === cartData?.article_list.length) {
       setSelectedItems([]);
     } else {
-      setSelectedItems(cartData?.article_list.map((item: any) => item.article) || []);
+      setSelectedItems(
+        cartData?.article_list.map((item: any) => item.article) || []
+      );
     }
   };
 
@@ -51,47 +53,60 @@ export default function Cart() {
     selectedItems.forEach((id) => {
       removeMutate(id, {
         onSuccess: () => {
-          queryClient.invalidateQueries({ queryKey: ['cart'] });
-        }
+          queryClient.invalidateQueries({ queryKey: ["cart"] });
+        },
       });
-    }
-    );
+    });
   };
 
   const handleOrder = () => {
     if (selectedItems.length === 0) return;
     const orderData = selectedItems.map((item) => ({
       product: item,
-      quantity: cartData?.article_list.find((articleItem: { article: string }) => articleItem.article === item)?.quantity
+      quantity: cartData?.article_list.find(
+        (articleItem: { article: string }) => articleItem.article === item
+      )?.quantity,
     }));
     orderMutate(orderData, {
       onSuccess: (data) => {
-        queryClient.invalidateQueries({ queryKey: ['cart'] });
+        queryClient.invalidateQueries({ queryKey: ["cart"] });
         router.push(`/order/${data?.data?._id}`);
-      }
+      },
     });
   };
 
-  const cartOrFavoriteStyle = 'flex items-center gap-1 text-lg md:text-2xl font-semibold m-1 text-gray-900 bg-gray-100 p-2 rounded-md hover:opacity-100';
+  const cartOrFavoriteStyle =
+    "flex items-center gap-1 text-lg md:text-2xl font-semibold m-1 text-gray-900 border-b-4 p-2 hover:opacity-100";
   return (
     <div className="flex flex-col gap-5 max-w-[1400px] mx-auto p-1">
       <div className="flex">
-        <button 
-          className={`${cartOrFavorite !== 'cart' && 'opacity-30 bg-white'} ${cartOrFavoriteStyle}`}
-          onClick={() => setCartOrFavorite('cart')}
+        <button
+          className={`${
+            cartOrFavorite !== "cart" && "opacity-30 border-none"
+          } ${cartOrFavoriteStyle}`}
+          onClick={() => setCartOrFavorite("cart")}
         >
           <CiShoppingCart className="text-xl md:text-2xl" />
           장바구니
         </button>
-        <button 
-          className={`${cartOrFavorite !== 'favorite' && 'opacity-30 bg-white'} ${cartOrFavoriteStyle}`}
-          onClick={() => setCartOrFavorite('favorite')}>
+        <button
+          className={`${
+            cartOrFavorite !== "favorite" && "opacity-30 border-none"
+          } ${cartOrFavoriteStyle}`}
+          onClick={() => setCartOrFavorite("favorite")}
+        >
           <IoHeartSharp className="text-xl md:text-2xl text-red-500" />
           좋아요한 상품
         </button>
       </div>
-      {isLoading || likeLoading ? <LoadingSpinner /> : (
-        cartOrFavorite === 'favorite' ? <Favorite likeList={likeList?.data?.articles} setCartOrFavorite={setCartOrFavorite} /> : (
+      {isLoading || likeLoading ? (
+        <LoadingSpinner />
+      ) : cartOrFavorite === "favorite" ? (
+        <Favorite
+          likeList={likeList?.data?.articles}
+          setCartOrFavorite={setCartOrFavorite}
+        />
+      ) : (
         <>
           {cartData?.article_list.length !== 0 ? (
             <>
@@ -100,7 +115,9 @@ export default function Cart() {
               </h2>
               <div className="flex justify-between">
                 <Checkbox
-                  isSelected={selectedItems.length === cartData?.article_list.length}
+                  isSelected={
+                    selectedItems.length === cartData?.article_list.length
+                  }
                   onChange={handleAllCheckClick}
                 >
                   <p className="text-sm font-semibold">전체 선택</p>
@@ -110,29 +127,55 @@ export default function Cart() {
                 </button>
               </div>
               <CheckboxGroup value={selectedItems}>
-                {cartData?.article_list?.map((articleItem: { article: string; quantity: number; _id: string }) => (
-                  <div className="flex" key={articleItem?.article}>
-                    <Checkbox value={articleItem?.article} isSelected={selectedItems.includes(articleItem.article)} onChange={() => handleItemCheck(articleItem?.article)} />
-                    <CartProductDetail articleId={articleItem?.article} quantity={articleItem?.quantity} setPrices={setPrices} />
-                  </div>
-                ))
-                }
+                {cartData?.article_list?.map(
+                  (articleItem: {
+                    article: string;
+                    quantity: number;
+                    _id: string;
+                  }) => (
+                    <div className="flex" key={articleItem?.article}>
+                      <Checkbox
+                        value={articleItem?.article}
+                        isSelected={selectedItems.includes(articleItem.article)}
+                        onChange={() => handleItemCheck(articleItem?.article)}
+                      />
+                      <CartProductDetail
+                        articleId={articleItem?.article}
+                        quantity={articleItem?.quantity}
+                        setPrices={setPrices}
+                      />
+                    </div>
+                  )
+                )}
               </CheckboxGroup>
-              <Button color="primary"
+              <Button
+                color="primary"
                 className="w-full mt-1"
                 isDisabled={totalPrice === 0}
                 onClick={handleOrder}
               >
-                <p>{totalPrice !== 0 && formatPrice(totalPrice)}{totalPrice !== 0 ? ' 결제하기' : '상품을 선택해주세요'}</p>
+                <p>
+                  {totalPrice !== 0 && formatPrice(totalPrice)}
+                  {totalPrice !== 0 ? " 결제하기" : "상품을 선택해주세요"}
+                </p>
               </Button>
             </>
           ) : (
             <div className="flex flex-col items-center gap-3 mt-10">
-              <h2 className="text-lg font-semibold m-1">장바구니에 담긴 상품이 없습니다.</h2>
+              <h2 className="text-lg font-semibold m-1">
+                장바구니에 담긴 상품이 없습니다.
+              </h2>
               <p className="text-gray-700 text-sm">상품을 추가해보세요.</p>
-              <button className="text-sm border p-1 rounded-md mt-3" onClick={()=>setCartOrFavorite('favorite')}>좋아요한 상품 보기</button>
+              <button
+                className="text-sm border p-1 rounded-md mt-3"
+                onClick={() => setCartOrFavorite("favorite")}
+              >
+                좋아요한 상품 보기
+              </button>
               <div className="mt-40 border-t-2">
-                <h2 className="text-lg font-semibold mt-2">이런 상품은 어떠세요?</h2>
+                <h2 className="text-lg font-semibold mt-2">
+                  이런 상품은 어떠세요?
+                </h2>
                 <div className="flex gap-2 mt-3">
                   <div>
                     <img src="https://via.placeholder.com/150" alt="product" />
@@ -173,8 +216,8 @@ export default function Cart() {
               </Link>
             </div>
           )}
-        </>)
+        </>
       )}
     </div>
   );
-};
+}
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,10 +1,16 @@
-'use client';
+"use client";
 
 import { Button, Input } from "@nextui-org/react";
 import { useForm } from "react-hook-form";
-import { usernameV, emailV, emailConfirmV,passwordV, passwordConfirmV } from '../validationRules';
+import {
+  usernameV,
+  emailV,
+  emailConfirmV,
+  passwordV,
+  passwordConfirmV,
+} from "../validationRules";
 import { SignupForm } from "../../../types/signupForm";
-import { ErrorMessage } from '@hookform/error-message';
+import { ErrorMessage } from "@hookform/error-message";
 import useSendMail from "@/hooks/useSendMail";
 import useConfirmMail from "@/hooks/useConfirmMail";
 import useUsernameCheck from "@/hooks/useUsernameCheck";
@@ -14,9 +20,15 @@ import Swal from "sweetalert2";
 import { IoPersonAddOutline } from "react-icons/io5";
 
 const Signup = () => {
-  const { register, handleSubmit, formState: { errors }, getValues, trigger } = useForm<SignupForm>({
-    mode: 'onChange', // 입력값이 변경될 때마다 유효성 검사
-    reValidateMode: 'onChange', // 입력값이 변경될 때마다 유효성 검사
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    getValues,
+    trigger,
+  } = useForm<SignupForm>({
+    mode: "onChange", // 입력값이 변경될 때마다 유효성 검사
+    reValidateMode: "onChange", // 입력값이 변경될 때마다 유효성 검사
   });
 
   // 이메일 인증시간 카운트다운
@@ -32,82 +44,86 @@ const Signup = () => {
 
   // 시간 포맷 함수
   const formatTime = useCallback((second: number | null) => {
-    if(second == null) return;
+    if (second == null) return;
     const min = Math.floor(second / 60);
     const sec = second % 60;
-    return `${min}:${sec.toString().padStart(2, '0')}`;
+    return `${min}:${sec.toString().padStart(2, "0")}`;
   }, []);
 
   // 카운트다운 처리
-  useEffect(()=> {
+  useEffect(() => {
     let timer: NodeJS.Timeout;
-    if(countdown !== null && countdown > 0) {
-      timer = setTimeout(() => setCountdown((prev)=> prev! - 1), 1000);
-    } else if(countdown === 0) {
+    if (countdown !== null && countdown > 0) {
+      timer = setTimeout(() => setCountdown((prev) => prev! - 1), 1000);
+    } else if (countdown === 0) {
       setCountdown(null);
       setExpriedMessage(true);
-    };
+    }
 
     return () => clearTimeout(timer);
   }, [countdown]);
 
   // 이메일 전송
-  const handleSendMail = async() => {
-    const isValid = await trigger('email');
-    if(isValid) {
-      const emailValue = getValues('email');
+  const handleSendMail = async () => {
+    const isValid = await trigger("email");
+    if (isValid) {
+      const emailValue = getValues("email");
       sendMail.mutate(emailValue, {
         onSuccess: () => {
           setCountdown(300);
           setExpriedMessage(false);
-        }
+        },
       });
-    };
+    }
   };
 
   // 이메일 인증
-  const handleMailConfirm = async() => {
-    const isValid = await trigger('emailConfirm');
-    if(isValid) {
-      const email = getValues('email');
-      const emailCode = getValues('emailConfirm').trim();
-      confirmMail.mutate({email, emailCode}, {
-        onSuccess: () => {
-          setCountdown(null);
-          setExpriedMessage(false);
+  const handleMailConfirm = async () => {
+    const isValid = await trigger("emailConfirm");
+    if (isValid) {
+      const email = getValues("email");
+      const emailCode = getValues("emailConfirm").trim();
+      confirmMail.mutate(
+        { email, emailCode },
+        {
+          onSuccess: () => {
+            setCountdown(null);
+            setExpriedMessage(false);
+          },
         }
-      });
-    };
+      );
+    }
   };
 
   // 유저이름 중복확인
-  const handleUsernameCheck = async() => { 
-    const isValid = await trigger('username');
-    if(isValid) {
-      const userName = getValues('username');
+  const handleUsernameCheck = async () => {
+    const isValid = await trigger("username");
+    if (isValid) {
+      const userName = getValues("username");
       usernameCheck.mutate(userName);
-    };
+    }
   };
 
   // 회원가입 요청
-  const onSubmit = (formData:SignupForm) => {
-    if(!confirmMail.isSuccess || !usernameCheck.isSuccess) {
+  const onSubmit = (formData: SignupForm) => {
+    if (!confirmMail.isSuccess || !usernameCheck.isSuccess) {
       Swal.fire({
-        icon: 'error',
-        title: '회원가입 실패',
-        text: '이메일 인증과 유저이름 중복확인을 완료해주세요.',
+        icon: "error",
+        title: "회원가입 실패",
+        text: "이메일 인증과 유저이름 중복확인을 완료해주세요.",
       });
       return;
-    };
-    const {email, password, username} = formData;
-    signup.mutate({email, password, username});
+    }
+    const { email, password, username } = formData;
+    signup.mutate({ email, password, username });
   };
 
-  const formInDiv = 'flex items-end gap-1';
-  const errorS = 'text-sm text-red-500';
+  const formInDiv = "flex items-end gap-1";
+  const errorS = "text-sm text-red-500";
   return (
     <div className="flex items-center justify-center h-[60vh] text-gray-800">
-      <form className="flex flex-col w-[500px] mx-auto gap-3 border p-3 rounded-md"
+      <form
+        className="flex flex-col w-[500px] mx-auto gap-3 border p-3 rounded-md"
         onSubmit={handleSubmit(onSubmit)}
       >
         <div className="flex items-center gap-2 text-2xl font-semibold m-1">
@@ -116,41 +132,43 @@ const Signup = () => {
         </div>
         {/* 이메일 입력&전송&에러메세지 */}
         <div className={formInDiv}>
-          <Input 
-            type="email" 
-            label="이메일" 
-            variant="underlined" 
+          <Input
+            type="email"
+            label="이메일"
+            variant="underlined"
             required
-            {...register('email', emailV)}
+            {...register("email", emailV)}
             isDisabled={confirmMail.isSuccess}
           />
-          <Button 
-            color="primary" 
-            onClick={handleSendMail} 
+          <Button
+            variant="bordered"
+            size="sm"
+            onClick={handleSendMail}
             isLoading={sendMail.isPending}
             isDisabled={confirmMail.isSuccess}
           >
             전송
           </Button>
         </div>
-        <ErrorMessage 
-          errors={errors} 
+        <ErrorMessage
+          errors={errors}
           name="email"
           render={({ message }) => <p className={errorS}>{message}</p>}
         />
         {/* 이메일 인증입력&확인&에러메세지 */}
         <div className={formInDiv}>
-          <Input 
-            type="text" 
-            label="이메일 인증" 
-            variant="underlined" 
+          <Input
+            type="text"
+            label="이메일 인증"
+            variant="underlined"
             required
-            {...register('emailConfirm', emailConfirmV)} 
+            {...register("emailConfirm", emailConfirmV)}
             isDisabled={confirmMail.isSuccess}
           />
           <p className="text-sm text-yellow-500 m-1">{formatTime(countdown)}</p>
-          <Button 
-            color="primary" 
+          <Button
+            variant="bordered"
+            size="sm"
             onClick={handleMailConfirm}
             isLoading={confirmMail.isPending}
             isDisabled={confirmMail.isSuccess}
@@ -163,72 +181,69 @@ const Signup = () => {
             인증시간이 만료되었습니다. 다시 인증해주세요.
           </p>
         )}
-        <ErrorMessage 
-          errors={errors} 
+        <ErrorMessage
+          errors={errors}
           name="emailConfirm"
           render={({ message }) => <p className={errorS}>{message}</p>}
         />
         {/* 유저이름 입력&중복확인&에러메세지 */}
         <div className={formInDiv}>
-          <Input 
-            type="text" 
-            label="유저이름" 
-            variant="underlined" 
+          <Input
+            type="text"
+            label="유저이름"
+            variant="underlined"
             required
-            {...register('username', usernameV)}  
+            {...register("username", usernameV)}
           />
-          <Button 
-            color="primary" 
+          <Button
+            variant="bordered"
+            size="sm"
             isLoading={usernameCheck.isPending}
             onClick={handleUsernameCheck}
           >
             중복확인
           </Button>
         </div>
-        <ErrorMessage 
-          errors={errors} 
+        <ErrorMessage
+          errors={errors}
           name="username"
           render={({ message }) => <p className={errorS}>{message}</p>}
         />
         {/* 비밀번호 입력&에러메세지 */}
-        <Input 
-          type="password" 
-          label="비밀번호" 
-          variant="underlined" 
+        <Input
+          type="password"
+          label="비밀번호"
+          variant="underlined"
           required
-          {...register('password', passwordV)} 
+          {...register("password", passwordV)}
         />
-        <ErrorMessage 
-          errors={errors} 
+        <ErrorMessage
+          errors={errors}
           name="password"
           render={({ message }) => <p className={errorS}>{message}</p>}
         />
         {/* 비밀번호 확인 입력&에러메세지 */}
-        <Input 
-          type="password" 
-          label="비밀번호 확인" 
-          variant="underlined" 
+        <Input
+          type="password"
+          label="비밀번호 확인"
+          variant="underlined"
           required
-          {...register('passwordConfirm', passwordConfirmV)} 
+          {...register("passwordConfirm", passwordConfirmV)}
         />
-        <ErrorMessage 
-          errors={errors} 
+        <ErrorMessage
+          errors={errors}
           name="passwordConfirm"
-          render={({ message }) => <p className={errorS}>{message}</p>} 
+          render={({ message }) => <p className={errorS}>{message}</p>}
         />
         {/* 회원가입 버튼 */}
         <div className="flex justify-end">
-          <Button 
-            type="submit"
-            variant="bordered"
-            isLoading={signup.isPending}
-          >
+          <Button type="submit" color="primary" isLoading={signup.isPending}>
             회원가입
           </Button>
         </div>
       </form>
     </div>
-  )
+  );
 };
 
 export default Signup;
