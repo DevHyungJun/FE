45e70ec2162fe0529e84efda7a7dd2321c59a8a5
jun <znznun@gmail.com>
2024-12-26 00:40:53 +0900

    update

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button, Input } from "@nextui-org/react";
+import { Button, Input, Select, SelectItem } from "@nextui-org/react";
 import { useForm } from "react-hook-form";
 import {
   usernameV,
@@ -37,6 +37,7 @@ const Signup = () => {
   // 인증시간 만료 메세지 여부
   const [expriedMessage, setExpriedMessage] = useState(false);
   const [gender, setGender] = useState("");
+  const [generation, setGeneration] = useState("");
 
   // 커스텀 훅 사용
   const sendMail = useSendMail();
@@ -108,20 +109,44 @@ const Signup = () => {
 
   // 회원가입 요청
   const onSubmit = (formData: SignupForm) => {
-    if (!confirmMail.isSuccess || !usernameCheck.isSuccess) {
+    if (
+      !confirmMail.isSuccess ||
+      !usernameCheck.isSuccess ||
+      gender === "" ||
+      generation === ""
+    ) {
       Swal.fire({
         icon: "error",
         title: "회원가입 실패",
-        text: "이메일 인증과 유저이름 중복확인을 완료해주세요.",
+        text: "모든 항목을 입력해주세요.",
       });
       return;
     }
     const { email, password, username } = formData;
-    signup.mutate({ email, password, username });
+    signup.mutate({
+      email,
+      password,
+      username,
+      gender,
+      generation: Number(generation),
+    });
   };
 
   const formInDiv = "flex items-end gap-1";
   const errorS = "text-sm text-red-500";
+
+  const generations = [
+    { key: 10, label: "10대" },
+    { key: 20, label: "20대" },
+    { key: 30, label: "30대" },
+    { key: 40, label: "40대" },
+    { key: 50, label: "50대" },
+    { key: 60, label: "60대" },
+    { key: 70, label: "70대" },
+    { key: 80, label: "80대" },
+    { key: 90, label: "90대" },
+  ];
+
   return (
     <div className="flex items-center justify-center h-[60vh] text-gray-800">
       <form
@@ -237,27 +262,39 @@ const Signup = () => {
           name="passwordConfirm"
           render={({ message }) => <p className={errorS}>{message}</p>}
         />
-        <div className="flex gap-2">
-          <button
-            className={`flex items-center bg-gray-100 ${
-              gender === "man" && "bg-blue-200"
-            } p-2 rounded-md text-gray-900 hover:bg-blue-200`}
+        <div className="flex gap-2 mb-5">
+          <div
+            className={`flex items-center border ${
+              gender === "man" && "bg-blue-100"
+            } p-2 rounded-md text-gray-900 select-none cursor-pointer`}
+            onClick={() => setGender("man")}
           >
             남성
-            <IoMdMan
-              className="text-2xl text-blue-500"
-              onClick={() => setGender("man")}
-            />
-          </button>
-          <button
-            className={`flex items-center bg-gray-100 ${
-              gender === "woman" && "bg-pink-200"
-            } p-2 rounded-md text-gray-900 hover:bg-pink-200`}
+            <IoMdMan className="text-2xl text-blue-500" />
+          </div>
+          <div
+            className={`flex items-center border ${
+              gender === "woman" && "bg-pink-100"
+            } p-2 rounded-md text-gray-900 select-none cursor-pointer`}
             onClick={() => setGender("woman")}
           >
             여성 <IoMdWoman className="text-2xl text-pink-500" />
-          </button>
+          </div>
         </div>
+        <Select
+          label="연령대"
+          placeholder="고객님의 연령대를 선택해주세요"
+          className="w-[50%]"
+          selectedKeys={[generation]}
+          onChange={(e) => setGeneration(e.target.value)}
+          variant="underlined"
+        >
+          {generations.map((gen) => (
+            <SelectItem key={gen.key} value={gen.key}>
+              {gen.label}
+            </SelectItem>
+          ))}
+        </Select>
         {/* 회원가입 버튼 */}
         <div className="flex justify-end">
           <Button type="submit" color="primary" isLoading={signup.isPending}>
