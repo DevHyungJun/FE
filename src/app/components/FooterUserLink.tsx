59fc5222ab86bf5879204e8a1620800a9e2a8f69
jun <znznun@gmail.com>
2025-07-21 15:30:48 +0900

    refactor: footer refactor

---
+++ src/app/components/FooterUserLink.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import Link from "next/link";
+import useAuthCheck from "@/hooks/useAuthCheck";
+
+const FooterUserLink = () => {
+  const { isSuccess } = useAuthCheck();
+  return (
+    <div>
+      {isSuccess && (
+        <>
+          <Link href="/cart" className="md:hover:text-[#D6D5D6]">
+            장바구니
+          </Link>
+          <Link href="/mypage" className="md:hover:text-[#D6D5D6]">
+            마이페이지
+          </Link>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default FooterUserLink;
