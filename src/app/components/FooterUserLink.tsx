d883bd3b2d086be6994d39fe3ac0d3101a097e1c
jun <znznun@gmail.com>
2025-07-21 15:32:22 +0900

    fix

--- src/app/components/FooterUserLink.tsx
+++ src/app/components/FooterUserLink.tsx
@@ -6,18 +6,18 @@ import useAuthCheck from "@/hooks/useAuthCheck";
 const FooterUserLink = () => {
   const { isSuccess } = useAuthCheck();
   return (
-    <div>
+    <>
       {isSuccess && (
-        <>
+        <div className="space-x-3">
           <Link href="/cart" className="md:hover:text-[#D6D5D6]">
             장바구니
           </Link>
           <Link href="/mypage" className="md:hover:text-[#D6D5D6]">
             마이페이지
           </Link>
-        </>
+        </div>
       )}
-    </div>
+    </>
   );
 };
 
