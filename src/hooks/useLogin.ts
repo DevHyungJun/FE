ae26515b3fc4e8686b0857f59c6d6da034f8cc28
jun <znznun@gmail.com>
2025-07-21 12:30:52 +0900

    test

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -4,6 +4,12 @@ import { Login } from "../../types/login";
 
 export default function useLogin() {
   return useMutation({
-    mutationFn: (loginData: Login)=> login(loginData)
+    mutationFn: (loginData: Login) => login(loginData),
+    onSuccess: () => {
+      alert("로그인 성공");
+    },
+    onError: () => {
+      alert("로그인 실패");
+    },
   });
 }
