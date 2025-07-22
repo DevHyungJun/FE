97f3ab888c5b4a8763cc8927f89591c113d4f9bc
jun <znznun@gmail.com>
2025-07-22 15:26:16 +0900

    fix

--- src/api/allProducts.ts
+++ src/api/allProducts.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export default async function allProducts(page = 1, category = "", limit = 10) {
+  const USER_ID = "681ca8bc0831b895114e5fed";
   const response = await axios.get(
-    `${process.env.NEXT_PUBLIC_API_URL}articles?limit=${limit}&page=${page}&category=${category}`
+    `${process.env.NEXT_PUBLIC_API_URL}articles?limit=${limit}&page=${page}&category=${category}&userId=${USER_ID}`
   );
   return response.data;
 }
