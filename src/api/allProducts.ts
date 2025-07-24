24da5fdf7b900996e6547525ba5dba1316b61d00
jun <znznun@gmail.com>
2025-07-25 01:28:59 +0900

    allProducts userId filter

--- src/api/allProducts.ts
+++ src/api/allProducts.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export default async function allProducts(page = 1, category = "", limit = 10) {
   const USER_ID = "681ca8bc0831b895114e5fed";
   const response = await axios.get(
-    `${process.env.NEXT_PUBLIC_API_URL}articles?limit=${limit}&page=${page}&category=${category}&userId=${USER_ID}`
+    `${process.env.NEXT_PUBLIC_API_URL}articles?limit=${limit}&page=${page}&category=${category}&user_id=${USER_ID}`
   );
   return response.data;
 }
